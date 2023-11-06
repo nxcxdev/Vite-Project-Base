5b85ded15495e7232f7acd442de72d9f53069427
Nicolás Albarenque Malnero <nxcxdev@gmail.com>
2023-11-06 12:14:23 +0100

    initialized repository

---
+++ src/pages/about/about.ts
@@ -0,0 +1,9 @@
+import '../../style.scss';
+
+console.log('about us');
+
+
+const app = document.getElementById('app') as HTMLDivElement
+
+app.innerHTML = /* html */ `
+    <h2>About Us</h2>`
---
+++ src/pages/contact/contact.ts
@@ -0,0 +1,8 @@
+import '../../style.scss'
+
+console.log('Contact');
+
+const app = document.getElementById('app') as HTMLDivElement
+
+app.innerHTML = /* html */ `
+    <h2>Contact</h2>`
